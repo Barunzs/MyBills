9a58fac6b7b0a9431b24ace6546b228e49a90245
Barun <bareen143@gmail.com>
2018-05-23 07:39:39 +0530

    Added PreviewActivity

--- app/src/main/java/bill/com/mybills/ui/activity/PreviewActivity.kt
+++ app/src/main/java/bill/com/mybills/ui/activity/PreviewActivity.kt
@@ -1,6 +1,13 @@
 package bill.com.mybills.ui.activity
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import bill.com.mybills.R
 
 class PreviewActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
 }
