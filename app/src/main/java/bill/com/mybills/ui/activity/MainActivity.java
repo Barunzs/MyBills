ac6bb5989db9d512141f82ad09c0aae3dd390804
Barun <bareen143@gmail.com>
2019-11-09 16:13:40 +0530

    Gradle Updated

--- app/src/main/java/bill/com/mybills/ui/activity/MainActivity.java
+++ app/src/main/java/bill/com/mybills/ui/activity/MainActivity.java
@@ -21,7 +21,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -49,7 +48,7 @@ import bill.com.mybills.ui.fragment.EditProfileFragment;
 import bill.com.mybills.ui.fragment.MyBillTransactionFragment;
 import bill.com.mybills.ui.fragment.MyProfileFragment;
 import bill.com.mybills.ui.fragment.ScanFragment;
-import io.fabric.sdk.android.Fabric;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -68,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         appDAL = new AppDAL(getApplicationContext());
-        Fabric.with(this, new Crashlytics());
+        //Fabric.with(this, new Crashlytics());
         mTitle = mDrawerTitle = getTitle();
         Toolbar topToolBar = findViewById(R.id.toolbar2);
         setSupportActionBar(topToolBar);
